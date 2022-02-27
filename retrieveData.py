fb5d63811ab893ff173c7824b83f7231fc019a58
David Pinto <davidpinto15@gmail.com>
2022-02-27 23:33:20 +0000

    added .env file and changed data type to dict

--- retrieveData.py
+++ retrieveData.py
@@ -1,18 +1,22 @@
 import json
 import os
+import re
+from dotenv import load_dotenv
+
+load_dotenv()
+DATAPATH = os.getenv('DATAPATH')
 
 
 def retrieveData():
-    PATH = "E:\\Downloads\\facebook-davidmarch15\\messages\\inbox\\joanaribeiro_oszc0ittog"
-
-    # TODO fix the bug of having several dicts inside and only reads the first
-    # Gets all the files names ended in json
-    files = [pos_json for pos_json in os.listdir(PATH) if pos_json.endswith('.json')]
+    # Gets all the files names ended in json, then gets just the number on the file name into a sorted list
+    files = [pos_json for pos_json in os.listdir(DATAPATH) if pos_json.endswith('.json')]
+    fileNumber = {}
+    for name in files:
+        fileNumber[name] = int([re.findall(r'(\w+?)(\d+)', name)[0]][0][1])
 
     # Searches all the files found above and loads the data from them
     jsonData = {}
-    for i, js in enumerate(files):
-        with open(os.path.join(PATH, js)) as json_file:
-            jsonData[files[i]] = json.load(json_file)['messages']
-
-    return jsonData
+    for js in files:
+        with open(os.path.join(DATAPATH, js)) as json_file:
+            jsonData[fileNumber[js]] = json.load(json_file)['messages']
+    return jsonData, files
